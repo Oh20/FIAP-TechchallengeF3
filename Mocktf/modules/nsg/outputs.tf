54cb5581b3f9f2d1e2c90efcdcaefb8363db984c
Antony Nascimento <Oh20Tony@outlook.com>
2026-08-31 19:13:33 +0000

    Deleted Mocktf

--- Mocktf/modules/nsg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nsg_id" {
-  description = "ID do Network Security Group"
-  value       = azurerm_network_security_group.this.id
-}
-
-output "nsg_name" {
-  description = "Nome do Network Security Group"
-  value       = azurerm_network_security_group.this.name
-}
